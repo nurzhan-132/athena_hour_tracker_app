01414b3e3a9f46020beae13d0e2f64208d42cbb9
Nurzhan Momynkul <nurzhanmomynov01@gmail.com>
2022-04-06 06:33:13 +0600

    Main Menu for User

---
+++ lib/Screens/Main-Menu/Components/main_menu_background_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainMenuBackgroundWidget extends StatelessWidget {
+  final Widget child;
+  const MainMenuBackgroundWidget({
+    Key? key, required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            bottom: 0,            
+            child: Image.asset(
+              "assets/images/main_bottom.png",
+              fit: BoxFit.cover,
+              //alignment: Alignment.bottomLeft,
+              height: size.height * 0.31,
+              width: size.width,
+            ),
+          ),
+          child,
+        ],
+      ),      
+    );
+  }
+}
